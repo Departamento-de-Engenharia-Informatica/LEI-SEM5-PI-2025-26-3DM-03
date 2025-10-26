1e940f12b88a0c2026e20c586b25937c4126c6ba
martimlemos <1221009@isep.ipp.pt>
2025-10-26 18:49:51 +0000

    documentation us2.2.11 done

--- TodoApi/Models/Staff/DTO/UpdateStaffDTO.cs
+++ TodoApi/Models/Staff/DTO/UpdateStaffDTO.cs
@@ -1,17 +1,17 @@
-using System;
-using System.Collections.Generic;
+    using System;
+    using System.Collections.Generic;
 
-namespace TodoApi.Models.Staff
-{
-    public class UpdateStaffDTO
+    namespace TodoApi.Models.Staff
     {
-        public string ShortName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
-        public TimeSpan? StartTime { get; set; }
-        public TimeSpan? EndTime { get; set; }
-        public string Status { get; set; } = string.Empty;
-        public bool Active { get; set; }
-        public List<string> Qualifications { get; set; } = new List<string>();
+        public class UpdateStaffDTO
+        {
+            public string ShortName { get; set; } = string.Empty;
+            public string Email { get; set; } = string.Empty;
+            public string PhoneNumber { get; set; } = string.Empty;
+            public TimeSpan? StartTime { get; set; }
+            public TimeSpan? EndTime { get; set; }
+            public string Status { get; set; } = string.Empty;
+            public bool Active { get; set; }
+            public List<string> Qualifications { get; set; } = new List<string>();
+        }
     }
-}
